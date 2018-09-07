f3732b695d3198f63232daa8193d4f1ce30b0790
Joy Goh-Mah <royran9er@gmail.com>
2018-09-07 23:14:25 +0100

    initial commit, sets up server

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require ('body-parser');
+
+
+const app = express();
+app.use(bodyParser.json());
+
+app.listen(3001, () => {
+  console.log('Beep boop! Server listening on port 3001!');
+});
+
+
+// Endpoints I need
+
+// REGISTER -- POST
+// Request body should have Name, username, password. This function should add user to
+// database, encrypt password and send it to the login table. Response should be the
+// user object.
+
+// SIGNIN -- POST
+// Request body should have username and password. Check against database. If successful,
+// response should be user object (including array of user's existing notes)
+
+// NEWNOTE -- POST
+// Request body should take userid and the contents of the note. This function should
+// create a new entry in the user's notes table. And should respond with the new note.
+
+// DELETENOTE -- DELETE
+// Request body should have noteid. Change the trashed property to true
+
+// EDITNOTE -- PUT
+//
