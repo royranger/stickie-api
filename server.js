8cdbd9196989b60d5f71a1425c080ebe03570ee3
Joy Goh-Mah <royran9er@gmail.com>
2018-09-12 23:59:02 +0100

    removes username from notes table

--- server.js
+++ server.js
@@ -103,11 +103,10 @@ app.post('/board', (req, res)=> {
 
 // BOARD, CREATE NEWNOTE
 app.post('/boardnewstickie', (req, res) => {
-  const {newnote, userid, username} = req.body;
+  const {newnote, userid} = req.body;
 
   db('notes').insert({
     userid: userid,
-    username: username,
     content: newnote,
     created: new Date()
   }).returning('*')
@@ -174,25 +173,3 @@ app.put('/boardnotecount', (req, res) => {
 app.listen(3001, () => {
   console.log('Beep boop! Server listening on port 3001!');
 });
-
-
-// Endpoints I need
-
-// REGISTER -- POST
-// Request body should have Name, username, password. This function should add user to
-// database, encrypt password and send it to the login table. Response should be the
-// user object.
-
-// SIGNIN -- POST
-// Request body should have username and password. Check against database. If successful,
-// response should be user object (including array of user's existing notes)
-
-// NEWNOTE -- POST
-// Request body should take userid and the contents of the note. This function should
-// create a new entry in the user's notes table. And should respond with the new note.
-
-// DELETENOTE -- DELETE
-// Request body should have noteid. Change the trashed property to true
-
-// EDITNOTE -- PUT
-//
